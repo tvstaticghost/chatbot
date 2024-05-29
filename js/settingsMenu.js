2d2c79eec86b00f151b09e438f9be78d6116e224
tvstaticghost <ray.m.arnold@gmail.com>
2024-05-29 08:02:16 -0400

    added functionality to the settings darkmode button

--- js/settingsMenu.js
+++ js/settingsMenu.js
@@ -1,5 +1,5 @@
 function settingsMenu () {
-    const gear = document.getElementById('settingsGear')
+    const gear = document.getElementById('settingsGear');
     const menuWindow = document.getElementById('menuWindow');
     const sliderContainer = document.getElementById('sliderContainer');
     const slider = document.getElementById('slider');
@@ -24,14 +24,44 @@ function settingsMenu () {
             sliderContainer.classList.add('justify-start');
             sliderContainer.classList.remove('light-blue-background');
             sliderContainer.classList.add('bg-gray-300');
+            toggleDarkMode('off');
         }
         else {
             sliderContainer.classList.remove('justify-start');
             sliderContainer.classList.add('justify-end');
             sliderContainer.classList.remove('bg-gray-300');
             sliderContainer.classList.add('light-blue-background');
+            toggleDarkMode('on');
         }
     })
+}
+
+//fix this function
+function toggleDarkMode(state) {
+    const header = document.getElementById('chatHeader');
+    const outputScreen = document.getElementById('conversationOutput');
+    const gear = document.getElementById('settingsGear');
+
+    if (state === 'off') {
+        document.body.classList.remove('text-slate-100');
+        document.body.classList.add('text-black');
+        header.classList.remove('blue-background');
+        header.classList.add('bg-white');
+        header.classList.add('border-2', 'border-black');
+        gear.classList.remove('white-filter');
+        outputScreen.classList.remove('blue-background');
+        outputScreen.classList.add('bg-gray-300');
+    }
+    else {
+        document.body.classList.remove('text-black');
+        document.body.classList.add('text-slate-100');
+        header.classList.remove('bg-white');
+        header.classList.add('blue-background');
+        header.classList.remove('border-2', 'border-black');
+        gear.classList.add('white-filter');
+        outputScreen.classList.remove('bg-gray-300');
+        outputScreen.classList.add('blue-background');
+    }
 }
 
 //intializing the settings menu
